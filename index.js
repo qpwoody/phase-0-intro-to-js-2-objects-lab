597674b03ab37c6d75b725cd83e06d268e505cd8
quinn woodhouse <qpwoodhouse@gmail.com>
2023-11-01 12:34:32 -0600

    objects lab

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+const employee = {
+    name: "Sam",
+    streetAddress: "12 Broadway"
+}
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    const newEmployee = {...employee}
+    newEmployee[key] = value
+    return newEmployee
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key] = value
+    return employee
+}
+function deleteFromEmployeeByKey(employee, key){
+    const newObj = {...employee}
+    delete newObj.name
+    return newObj
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee.name
+    return employee
+}
